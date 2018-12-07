8de0f1150542d181b514be33a6fd61253d24458b
Bo Chen <bochentheone@gmail.com>
2018-12-07 16:36:58 -0500

    change site icon

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,7 +3,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/bochen/Documents/gatsby-starter-dimension/node_modules/gatsby-plugin-offline/app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/bochen/Documents/gatsby-starter-dimension/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/bochen/Documents/gatsby-starter-dimension/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/bochen/Documents/gatsby-starter-dimension/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/bochen/Documents/gatsby-starter-dimension/src/pages/page-2.js"))
