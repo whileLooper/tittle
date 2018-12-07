398d2e2cb43bfc0b0d502311e3d09de1e02a778f
Bo Chen <bochentheone@gmail.com>
2018-12-07 12:06:34 -0500

    first build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/bochen/Documents/gatsby-starter-dimension/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/Users/bochen/Documents/gatsby-starter-dimension/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
   "component---src-pages-404-js": () => import("/Users/bochen/Documents/gatsby-starter-dimension/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("/Users/bochen/Documents/gatsby-starter-dimension/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-page-2-js": () => import("/Users/bochen/Documents/gatsby-starter-dimension/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
